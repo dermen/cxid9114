0b2741ec5d7c37f7f181c59b45969464bb3292e0
Derek Mendez <dermen@lbl.gov>
2019-08-21 11:51:44 -0700

    mpi rank 0 to pass imported functions to other ranks

--- io/filter_bboxes.py
+++ io/filter_bboxes.py
@@ -1,73 +1,139 @@
 
+
 from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.rank
+size = comm.size
 
-import h5py
-from cxid9114.integrate import integrate_utils
+# import functions on rank 0 only
+if rank==0:
+    from h5py import File as h5py_File
+    from cxid9114.integrate.integrate_utils import Integrator
+    from numpy import array, sqrt, percentile
+    from numpy import zeros as np_zeros
+    from numpy import sum as np_sum
+    from psutil import Process
+    from os import getpid
+else:
+    h5py_File = None
+    Integrator = None
+    array = sqrt = percentile = np_zeros = np_sum = None
+    Process = None
+    getpid = None
 
-import numpy as np
-
-# 
-# size = MPI.COMM_WORLD.size
-
-import psutil
-import os
+h5py_File = comm.bcast(h5py_File, root=0)
+Integrator = comm.bcast(Integrator, root=0)
+array = comm.bcast(array, root=0)
+sqrt = comm.bcast(sqrt, root=0)
+percentile = comm.bcast(percentile, root=0)
+np_zeros = comm.bcast(np_zeros, root=0)
+np_sum = comm.bcast(np_sum, root=0)
+Process = comm.bcast(Process, root=0)
+getpid = comm.bcast( getpid, root=0)
 
 def get_memory_usage():
     """Return the memory usage in Mo."""
-    process = psutil.Process(os.getpid())
+    process = Process(getpid())
     mem = process.memory_info()[0] / float(2 ** 20)
     return mem
 
-@profile
+#def mpi_sum(value):
+#    global_sum = np_zeros(1, dtype='float64')
+#    local_sum = np_sum(array(value)).astype('float64')
+#    comm.Reduce(local_sum, global_sum, op=MPI.SUM)
+#    return global_sum[0]
+
+#@profile
 def main():
-    rank = MPI.COMM_WORLD.rank
 
-    h = h5py.File("/global/project/projectdirs/lcls/dermen/process_rank%d.h5" % rank, "r")
-    paths = h["h5_path"][()]
-    bbox_datagrp = h["bboxes"]
+    # data is stored in 39 h5py_Files
+    Nfiles = 39
+    fname_template = "/global/project/projectdirs/lcls/dermen/process_rank%d.h5"
+    fnames = [fname_template % i_f for i_f in range( Nfiles)]
 
-    Ntot = 0
-    all_tot_pix = []
+    # NOTE: for reference, inside each h5 file there is 
+    #   [u'Amatrices', u'Hi', u'bboxes', u'h5_path']
     
-    for img_num, img_path in enumerate(paths[:5]):
-        print "rank %d, memory usage = %.3f Mo" % (rank, get_memory_usage())
+    # get the total number of shots using worker 0
+    if rank==0:
+        print("I am root. I am calculating total number of shots")
+        h5s = [h5py_File(f, "r") for f in fnames]
+        Nshots_per_file = [ h["h5_path"].shape[0] for h in h5s]
+        Nshots_tot = sum(Nshots_per_file)
+        print("I am root. Total number of shots is %d" % Nshots_tot)
 
-        img_h5 = h5py.File(img_path, "r")
-        #img_data = img_h5["data32"][()]
-        img_data = img_h5["bigsim_d9114"][()]
+        print("I am root. I will divide shots amongst workers.")
+        shot_tuples = []
+        for i_f, fname in enumerate( fnames):
+            fidx_shotidx = [(i_f, i_shot) for i_shot in range(Nshots_per_file[i_f])] 
+            shot_tuples += fidx_shotidx
+
+        from numpy import array_split
+        print ("I am root. Number of uniques = %d" % len(set(shot_tuples)))
+        shots_for_rank = array_split( shot_tuples, size)
+
+    else:
+        Nshots_tot = None
+        shots_for_rank = None
+        h5s = None
+    
+    #Nshots_tot = comm.bcast( Nshots_tot, root=0)
+    shots_for_rank = comm.bcast( shots_for_rank, root=0)
+    #h5s = comm.bcast( h5s, root=0)  # pull in the open hdf5 files
+    
+    my_shots =  shots_for_rank[rank][:10]
+
+    # open the unique filenames for this rank
+    # TODO: check max allowed pointers to open hdf5 file
+    my_unique_fids = set([fidx for fidx,_ in my_shots])
+    my_open_files = {fidx: h5py_File( fnames[fidx], "r") for fidx in my_unique_fids}
+    
+    Ntot = 0
+    all_kept_bbox = []
+    for img_num, ( fname_idx, shot_idx) in  enumerate( my_shots):
+
+        h = my_open_files[fname_idx]
         
-        bboxes = bbox_datagrp["shot%d" % img_num][()]
-
-        #data_bbs = [img_data[ j1:j2, i1:i2] for i1,i2,j1,j2 in bboxes]
-        I = integrate_utils.Integrator(img_data, int_radius=5, gain=28)
+        # load the dxtbx image data directly:
+        img_path = h["h5_path"][shot_idx]
+        img_h5 = h5py_File(img_path, "r")
+        img_data = img_h5["bigsim_d9114"][()]  # LZF decompression, but not a bottleneck
+       
+        bboxes = h["bboxes"]["shot%d" % shot_idx][()]
+        
+        I = Integrator(img_data, int_radius=5, gain=28)
         int_data = [I.integrate_bbox_dirty( bb) for bb in bboxes]
-
+        
         # signal, background, variance
-        s,b,var = map(np.array, zip(*int_data) )
-        snr = s / np.sqrt(var)
+        s,b,var = map(array, zip(*int_data) )
+        snr = s / sqrt(var)
 
         # keep the top 10% 
-        is_a_keeper =  snr > np.percentile(snr, 90)
+        is_a_keeper =  snr > percentile(snr, 90)
 
-        data_boxes = [ img_data[j1:j2, i1:i2] for i_bb,(i1,i2,j1,j2) in enumerate(bboxes) if is_a_keeper[i_bb]]
+        kept_bboxes = [bboxes[i_bb] for i_bb in range(len(bboxes)) if is_a_keeper[i_bb]]
 
-        tot_pix = [ (j2-j1)*(i2-i1) for i_bb,(i1,i2,j1,j2) in enumerate(bboxes) if is_a_keeper[i_bb]]
+        data_boxes = [ img_data[j1:j2, i1:i2] for i_bb,(i1,i2,j1,j2) in enumerate(kept_bboxes)]
+
+        tot_pix = [ (j2-j1)*(i2-i1) for i_bb,(i1,i2,j1,j2) in enumerate(kept_bboxes)]
         Ntot += sum(tot_pix)
 
-        print "%g total pixels (file %d / %d)" % (Ntot, img_num+1, len(paths))
-        all_tot_pix +=  tot_pix
+        print "%g total pixels (file %d / %d)" % (Ntot, img_num+1, len(my_shots))
+        all_kept_bbox += map(list, kept_bboxes)
     
-    print "Rank %d; total bboxes=%d; Total pixels=%g" % (rank, len(all_tot_pix), Ntot)
-    all_tot_pix = MPI.COMM_WORLD.gather( all_tot_pix, root=0)
+    print("END OF LOOP")
+    print "Rank %d; total bboxes=%d; Total pixels=%g" % (rank, len(all_kept_bbox), Ntot)
+    all_kept_bbox = MPI.COMM_WORLD.gather(all_kept_bbox, root=0)
     
     if rank==0:
-        all_tot_pix = [tot_pix for SL in all_tot_pix for tot_pix in SL]
-        Ntot_pix = sum([ (j2-j1)*(i2-i1) for i1,i2,j1,j2 in bboxes])
+        all_kept_bbox = [bbox for bbox_lst in all_kept_bbox for bbox in bbox_lst]
+        Ntot_pix = sum([ (j2-j1)*(i2-i1) for i1,i2,j1,j2 in all_kept_bbox])
         print
         print("<><><><><><><<><><><><><><><><><><><><><><>")
-        print("Rank %d; total bboxes=%d, Total pixels=%g" % (rank, len(all_tot_pix), Ntot_pix))
+        print("I am root. total bboxes=%d, Total pixels=%g" % (len(all_kept_bbox), Ntot_pix))
         print("<><><><><><><<><><><><><><><><><><><><><><>")
         print
+
 
 if __name__=="__main__" :
     main()
