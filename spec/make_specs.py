4b1d8f45a9b9916bed1c03459aa05aa4f2141e2d
Derek Mendez <dermen@lbl.gov>
2020-04-23 17:20:52 -0700

    uposdasd

--- spec/make_specs.py
+++ spec/make_specs.py
@@ -1,3 +1,12 @@
+
+from argparse import ArgumentParser
+parser = ArgumentParser()
+
+parser.add_argument("-N", type=int, default=1000, help="Number of spectra to generate")
+parser.add_argument("--flux", type=float, default=8e10, help="average spectra flux")
+parser.add_argument("-o", type=str, required=True, help="output file name")
+args = parser.parse_args()
+
 # coding: utf-8
 from LS49.spectra.generate_spectra import spectra_simulation
 import h5py
@@ -5,10 +14,10 @@ import numpy as np
 from cxid9114.parameters import ENERGY_HIGH
 
 SS = spectra_simulation()
-total_flux = 1e12 #8e10
-Nspec = 111
+total_flux = args.flux
+Nspec = args.N
 E = ENERGY_HIGH
-E = 9476  # cyto data
+#E = 9476  # cyto data
 #SS.generate_recast_renormalized_images)2-, energy=
 spectras = SS.generate_recast_renormalized_images(
         Nspec, energy=E, total_flux=total_flux)
@@ -25,8 +34,7 @@ comp_args = {"compression":"gzip",
 
 from cxid9114.parameters import ENERGY_CONV
 energies = np.vstack([ENERGY_CONV / W for W in waves])
-#with h5py.File("bs7_100kspec.h5", "w") as f:
-with h5py.File("cyto_111spec.h5", "w") as f:
+with h5py.File(args.o, "w") as f:
     f.create_dataset("wavelengths", data=waves.astype(np.float32), **comp_args)
     f.create_dataset("energies", data=energies.astype(np.float32), **comp_args)
     f.create_dataset("fluxes", data=fluxes.astype(np.float32), **comp_args)
