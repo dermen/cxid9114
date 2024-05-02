604bb960ffdd0b1868238f8a141cea59f61a3ba3
dermen <dermen@lbl.gov>
2024-05-01 21:39:23 -0700

    updates

--- sim/perl_scale.sh
+++ sim/perl_scale.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=12
 #SBATCH --gpus-per-node=4
 #SBATCH --cpus-per-gpu=3
-#SBATCH -t 180
+#SBATCH -t 12
 #SBATCH -q regular 
 #SBATCH -C gpu
 #SBATCH -A m4326_g
@@ -23,7 +23,7 @@ SRUN="srun -N30 --ntasks-per-node=12 --gpus-per-node=4 --cpus-per-gpu=3 -c2"
 libtbx.python d9114_mpi_sims.py  -odir $ODIR --bg-name mybackground.h5 --make-background  --sad 
 
 # N process full image simulations (writes 10 images per rank in the some_images folder)
-$SRUN libtbx.python d9114_mpi_sims.py  -o test -odir ${ODIR}/some_images --add-bg --add-noise --profile gauss --bg-name ${ODIR}/mybackground.h5 -trials 10  --oversample 0 --Ncells 10 --xtal_size_mm 0.00015 --mos_doms 50 --mos_spread_deg 0.01  --saveh5 --readout  --masterscale 1150 --sad --bs7real --masterscalejitter 115 -g 4 --gpu
+$SRUN libtbx.python d9114_mpi_sims.py  -o test -odir ${ODIR}/some_images --add-bg --add-noise --profile gauss --bg-name ${ODIR}/mybackground.h5 -trials 20  --oversample 0 --Ncells 10 --xtal_size_mm 0.00015 --mos_doms 50 --mos_spread_deg 0.01  --saveh5 --readout  --masterscale 1150 --sad --bs7real --masterscalejitter 115 -g 4 --gpu
 
 sleep 30
 
@@ -66,3 +66,10 @@ sleep 30
 # run stage 2 ensemble refinement
 $SRUN simtbx.diffBragg.stage_two stage_two.phil io.output_dir=${ODIR}/stage2 pandas_table=${ODIR}/stage1/predict/preds_for_hopper.pkl num_devices=4 simulator.structure_factors.mtz_name=${ODIR}/merged/iobs_all.mtz
 
+
+#DIFFBRAGG_USE_CUDA=1 srun -N1 --ntasks-per-node=32 --cpus-per-gpu=8 --gpus-per-node=4 -c2 -n32   ens.hopper  watchmans_ease/hopper13_fall23/filtered2.pkl hopper13_fall23.phil --outdir test.ens --maxSigma 10 --saveFreq 10 --cmdlinePhil max_process=128 fix.Fhkl=False fix.G=False fix.RotXYZ=True fix.Nabc=True fix.ucell=True sigmas.G=0.1 --refl predictions
+
+
+DIFFBRAGG_USE_CUDA=1 srun -N4 --ntasks-per-node=32 -c2 --gpus-per-node=4 --cpus-per-gpu=8 -n16 ens.hopper  watchmans_ease/hopper13_fall23/33perc_filt.pkl hopper13_fall23.phil --outdir watchmans_ease/hopper13_fall23/33perc_Nref --saveFreq 10 --cmdlinePhil max_process=512 fix.Fhkl=False fix.G=False fix.RotXYZ=True fix.Nabc=False fix.ucell=True  sigmas.G=0.1 fix.diffuse_sigma=True fix.diffuse_gamma=True init.diffuse_sigma=[.12,.11,.1] init.diffuse_gamma=[1e3,1e3,1e3] diffuse_stencil_size=1 sigmas.diffuse_gamma=[.7,.7,.7] sigmas.diffuse_sigma=[.5,.5,.5] diffuse_orientation=1 use_diffuse_models=False logfiles=True  --refl predictions --saveAll
+
+DIFFBRAGG_USE_CUDA=1 srun -N4 --ntasks-per-node=32 -c2 --gpus-per-node=4 --cpus-per-gpu=8 -n16 ens.hopper  watchmans_ease/hopper13_fall23/33perc_filt.pkl hopper13_fall23.phil --outdir watchmans_ease/hopper13_fall23/33perc_Dref --saveFreq 10 --cmdlinePhil max_process=512 fix.Fhkl=False fix.G=False fix.RotXYZ=True fix.Nabc=True fix.ucell=True  sigmas.G=0.1 fix.diffuse_sigma=False fix.diffuse_gamma=False init.diffuse_sigma=[.12,.11,.1] init.diffuse_gamma=[1e3,1e3,1e3] diffuse_stencil_size=1 sigmas.diffuse_gamma=[.7,.7,.7] sigmas.diffuse_sigma=[.5,.5,.5] diffuse_orientation=1 use_diffuse_models=True logfiles=True  --refl predictions --saveAll
